915059a965e30b312fa95b7d09d2b29177a2f36d
AlphaDelta38 <kiril.morozov.01@gmail.com>
2025-04-22 19:28:00 +0300

    fix github actions settings

--- homework4/test/task/test_orm_constants_templates.py
+++ homework4/test/task/test_orm_constants_templates.py
@@ -1,19 +1,19 @@
 from homework3.part1.orm_constants_templates import (
-    UNIQUES, VARCHAR, DEFAULT, ENUM
+    create_uniques, create_varchar, create_default, create_enum
 )
 import pytest
 
 
 def test_uniques():
     input_data = ["name", "surname"]
-    result = UNIQUES(input_data)
+    result = create_uniques(input_data)
     expected = "UNIQUE(name, surname)"
     assert result == expected
 
 
 @pytest.mark.parametrize("input_data, expected", [(255, 255), (1, 1), (-1, 1), (256, 255)])
 def test_varchar(input_data, expected):
-    result = VARCHAR(input_data)
+    result = create_varchar(input_data)
     assert result == f"VARCHAR({expected})"
 
 @pytest.mark.parametrize("input_data, expected", [
@@ -21,11 +21,11 @@ def test_varchar(input_data, expected):
     (["test", ["one"], ""], "CHECK (test IN (\"one\") )")
 ])
 def test_enum(input_data, expected):
-    result = ENUM(*input_data)
+    result = create_enum(*input_data)
     assert result == expected
 
 
 @pytest.mark.parametrize("input_data, expected", [("NULL", "DEFAULT NULL"), ("\"TEST\"", "DEFAULT \"TEST\"")])
 def test_default(input_data, expected):
-    result = DEFAULT(input_data)
+    result = create_default(input_data)
     assert result == expected
